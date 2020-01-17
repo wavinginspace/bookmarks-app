098b2b56fdbba776d7eed167987f876c2fbd8e2d
wavinginspace <paulbaisley@gmail.com>
2020-01-17 17:09:44 -0500

    greybox wireframes done

--- scripts/store.js
+++ scripts/store.js
@@ -1,6 +1,12 @@
 import api from './api.js';
 import bookmarks from './bookmarks.js';
 
+const bookmarkList = [];
+let adding = false;
+let error = null;
+let filter = 0;
+
+
 export default {
-  
+  bookmarkList
 };
