022f61f9f9ccf1af8bd2a5fd9f714d5deab314e7
wavinginspace <paulbaisley@gmail.com>
2020-01-20 00:00:27 -0500

    minor edits

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -80,7 +80,7 @@ const generateInitialView = function() {
   <form id="bookmarks-form"></form>
   <label aria-label="filter by rating" for="filter-dropdown"></label>
   <select name="ratings" id="filter-dropdown">
-  <option value="1">Filter By Rating</option>
+  <option value="0">Filter By Rating</option>
   <option value="5">&#9733; &#9733; &#9733; &#9733; &#9733; or more</option>
   <option value="4">&#9733; &#9733; &#9733; &#9733; or more</option>
   <option value="3">&#9733; &#9733; &#9733; or more</option>
@@ -131,14 +131,14 @@ const generateAddBookmarkView = function() {
   <form id="add-new-bookmark-form" autocomplete="off">
 
     <label aria-label="new bookmark title" class="new-bookmark-label" for="new-bookmark-title">Add new bookmark title:</label>
-    <input class="new-bookmark-input" type="text" name="title" placeholder="New bookmark" id="new-bookmark-title">
+    <input class="new-bookmark-input" type="text" name="title" placeholder="New bookmark" id="new-bookmark-title" required>
 
     <label aria-label="new bookmark url" class="new-bookmark-label" for="new-bookmark-url"> Enter bookmark url: </label>
-    <input class="new-bookmark-input" type="text" name="url" placeholder="https://www.newbookmark.com" id="new-bookmark-url">
+    <input class="new-bookmark-input" type="text" name="url" placeholder="https://www.newbookmark.com" id="new-bookmark-url" required>
 
     <label aria-label="new-bookmark-rating" class="new-bookmark-label" for="new-bookmark-rating">Add bookmark rating:</label>
     <span>
-    <select class="new-bookmark-rating" name="add-rating" id="new-bookmark-rating" multiple>
+    <select class="new-bookmark-rating" name="add-rating" id="new-bookmark-rating" multiple required>
     <option value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
@@ -148,7 +148,7 @@ const generateAddBookmarkView = function() {
   </span>
 
     <label aria-label="new bookmark description" class="new-bookmark-label" for="new-bookmark-description"> Add description:</label>
-    <textarea cols="20" rows="5" class="new-bookmark-input" type="text" name="description" id="new-bookmark-description"></textarea>
+    <textarea cols="20" rows="5" class="new-bookmark-input" type="text" name="description" id="new-bookmark-description" required></textarea>
 
     <input class="add-new-bookmark-button form-button" type="submit">
 
@@ -230,7 +230,7 @@ const handleRatingsSelection = function () {
     const filterValue = parseInt($('#filter-dropdown').val());
     store.filter = filterValue;
     store.bookmarkList.forEach(item => item.expanded = false);
-    // ! THIS IS WORKING BUT  GIVING AN TYPEERROR 
+    // ! THIS IS WORKING BUT GIVING AN TYPEERROR + doesn't actually filter
     // let dropdown = document.getElementById('filter-dropdown');
     // dropdown.textContent = dropdown.value();
     render();
